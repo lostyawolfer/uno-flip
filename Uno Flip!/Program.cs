b0c7a8fbc6356b527b6f9ef0205a72a852d13149
Lostya <lostyawolfer>
2025-02-18 14:55:44 +0100

    initial commit

---
+++ Uno Flip!/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+    }
+
+    static float GetFloat(string question)
+    {
+        float result;
+        string input;
+
+        do
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write(question);
+            Console.ForegroundColor = ConsoleColor.White;
+            input = Console.ReadLine();
+        } while (!float.TryParse(input, out result));
+
+        return result;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Uno Flip!
+lostyawolfer's Uno Flip! implementation in console for 5TTI C# course finals
